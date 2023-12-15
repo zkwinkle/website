2352f0f558044057bcb2c86234dacdc3b3e9a5df
Ignacio Vargas <ignaevc@gmail.com>
2023-12-15 10:20:09 -0600

    Append unix time to stylesheet so it's always refreshed

--- src/css.rs
+++ src/css.rs
@@ -1,3 +1,6 @@
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+
 use maud::{html, Markup, Render};
 
 /// Links to a CSS stylesheet at the given path.
@@ -9,8 +12,13 @@ impl Css {
 
 impl Render for Css {
     fn render(&self) -> Markup {
+        let link_with_timestamp: String = match SystemTime::now().duration_since(UNIX_EPOCH) {
+            Ok(n) => format!("{}?{}", self.0, n.as_secs().to_string()),
+            Err(_) => String::from(self.0),
+        };
+
         html! {
-            link rel="stylesheet" type="text/css" href=(self.0);
+            link rel="stylesheet" type="text/css" href=(link_with_timestamp);
         }
     }
 }
