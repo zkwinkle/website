ee47da96a8ac7e79250157ba96120237363d3c8d
Ignacio Vargas <ignaevc@gmail.com>
2023-12-15 14:42:49 -0600

    Add home page content

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -6,7 +6,53 @@ use crate::components::headings;
 pub async fn home(uri: Uri) -> Markup {
     html! {
         (headings(uri))
-        h1 { "Hello, World!" }
-        p { "Roboto Lorem Ipsum!" }
+        div { div class="home-banner" {
+            img src="pfp.png";
+            div {
+                h1 { "zkwinkle" }
+                p { "Computer engineer in the making, an avid learner with too
+                    many interests and too little time."}
+            }
+        } }
+
+        h1 { "Info" }
+        p { "I'm Ignacio, or " i { "zkwinkle" } ", as I often go by in online spaces.
+        I'm a student engineer, with an interest in the fields of Embedded (or any kind of low-level development), Graphics, Machine Learning, and Signal Analysis/Processing. I'm a big Rust🦀 enthusiast, also I use Arch BTW."
+            br; br;
+        "So welcome to my website / digital garden / virtual world! Here I'll be hosting my blog as well as any
+        other silly little ideas I might wanna share with the world."
+        }
+
+        h1 { "Projects" }
+
+        p { "These are my personal projects I'm most proud of:" }
+        ul {
+            li { a href="https://github.com/zkwinkle/website"  {"This website!"} }
+            li { a href="https://github.com/zkwinkle/raytracer_ini"  {"Rust Raytracer"} }
+            li { a href="https://github.com/zkwinkle/Emulador-MIPS"  {"MIPS Emulator"} }
+        }
+
+        h1 { "Favorites" }
+
+        p { "Here's a few of my favorites, so you can get to know me better!" }
+
+        h2 { "Favorite videogames" }
+        ul {
+            li { "Hollow Knight 🪲" }
+            li { "Antichamber 🧩" }
+            li { "Team Fortress 2 🔥" }
+            li { strike{"League of Legends 🙃"} }
+            li { "Zelda (in general) 🧝🏻‍♀️" }
+        }
+
+        h2 { "Favorite animes" }
+        ul {
+            li { "Trigun 🔫" }
+            li { "Code Geass 🤖" }
+            li { "Hunter x Hunter 🎣" }
+            li { "Kill la Kill ✂️" }
+            li { "Princess Mononoke 🐺" }
+        }
+
     }
 }
