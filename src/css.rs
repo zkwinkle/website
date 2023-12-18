347105b3aeef24d5d1b3c15ee1fbda4575562b62
Ignacio Vargas <ignaevc@gmail.com>
2023-12-18 16:21:47 -0600

    Let stylesheet get cached in production

--- src/css.rs
+++ src/css.rs
@@ -11,7 +11,14 @@ pub struct Css(&'static str);
 impl Render for Css {
     fn render(&self) -> Markup {
         let link_with_timestamp: String = match SystemTime::now().duration_since(UNIX_EPOCH) {
-            Ok(n) => format!("{}?{}", self.0, n.as_secs().to_string()),
+            Ok(n) => {
+                // Add timestamp in development to reload stylesheet
+                if !cfg!(feature = "production") {
+                    format!("{}?{}", self.0, n.as_secs().to_string())
+                } else {
+                    self.0.to_owned()
+                }
+            }
             Err(_) => String::from(self.0),
         };
 
