d1f840cddb926c0afbceb49cdc1f7d8357270c8c
Ignacio Vargas <ignaevc@gmail.com>
2023-12-18 16:04:25 -0600

    Add production flag to nix build and set env var

--- default.nix
+++ default.nix
@@ -1,11 +1,17 @@
-{ pkgs }:
+{ pkgs, makeWrapper }:
 pkgs.rustPlatform.buildRustPackage rec {
 	pname = "website";
 	version = "0.1";
 	cargoLock.lockFile = ./Cargo.lock;
 	src = pkgs.lib.cleanSource ./.;
+	nativeBuildInputs = [
+		makeWrapper
+	];
+	buildFeatures = [ "production" ];
 	postInstall = ''
-cp -r public $out/public
-  '';
+		cp -r public $out/public
+		wrapProgram $out/bin/website \
+		--set PUBLIC_DIR "$out/public"
+		'';
 
 }
