875efa7d60639e4fb2e09a19de7ae8c838a4f656
Ignacio Vargas <ignaevc@gmail.com>
2023-12-23 01:18:23 -0600

    Format default.nix

--- default.nix
+++ default.nix
@@ -1,17 +1,16 @@
 { pkgs, makeWrapper }:
 pkgs.rustPlatform.buildRustPackage rec {
-	pname = "website";
-	version = "0.1";
-	cargoLock.lockFile = ./Cargo.lock;
-	src = pkgs.lib.cleanSource ./.;
-	nativeBuildInputs = [
-		makeWrapper
-	];
-	buildFeatures = [ "production" ];
-	postInstall = ''
-		cp -r public $out/public
-		wrapProgram $out/bin/website \
-		--set PUBLIC_DIR "$out/public"
-		'';
-
+  pname = "website";
+  version = "0.1";
+  cargoLock.lockFile = ./Cargo.lock;
+  src = pkgs.lib.cleanSource ./.;
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+  buildFeatures = [ "production" ];
+  postInstall = ''
+    		cp -r public $out/public
+    		wrapProgram $out/bin/website \
+    		--set PUBLIC_DIR "$out/public"
+    		'';
 }
